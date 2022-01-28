600264f440b7e393c2fd7f4816821120ef8ff908
nicolasarruda <nlarruda@hotmail.com>
2022-01-28 19:55:39 -0300

    User by id

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -31,7 +31,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Value("${jwt.duration}")
 	private Integer jwtDuration;
 	
-	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
@@ -45,7 +44,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	private AuthenticationManager authenticationManager;
 	
 	@Autowired
-	private JwtTokenEnhancer tokenEnhancer;
+	private JwtTokenEnhancer jwtTokenEnhancer;
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -66,7 +65,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		
 		TokenEnhancerChain chain = new TokenEnhancerChain();
-		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));
+		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, jwtTokenEnhancer));
 		
 		endpoints.authenticationManager(authenticationManager)
 		.tokenStore(tokenStore)
